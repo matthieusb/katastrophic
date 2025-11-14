1287d187de47fa4f0efb9378e3de3d9d8c77bbca
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2025-11-14 17:53:29 +0100

    feat: wip on java 25

---
+++ starters/technical-starters/java/25/maven/src/test/java/fr/kata/MainTest.java
@@ -0,0 +1,18 @@
+package fr.kata;
+
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("Main Test")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class MainTest {
+    // TODO
+}
